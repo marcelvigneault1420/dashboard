5ee06850f0129bee268aff66e07ff1c143efbd3a
Marcel <marcel.vigneault.1420@gmail.com>
2019-10-28 22:08:34 -0400

    Revert CD CI modification and trying an alternative.

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) => {
         next();
     } catch (error) {
         res.status(403).json({
-            error: { message: 'Access denied, no JWT token given' }
+            error: { message: 'Access denied' }
         });
     }
 };
